8df300b221d1d9cd21105a423e2b03a9db30b376
Nhan Thanh Trúc <42568753+nhanthanhtruc@users.noreply.github.com>
2018-08-28 16:42:01 +0700

    First

---
+++ TravelMoney/TravelMoney/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TravelMoney.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace TravelMoney
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
